db58b94c8290f64d6f1200bc9dc11188a1e370ca
AnneWhit <92681743+AnneWhit@users.noreply.github.com>
2022-07-02 10:00:01 -0400

    Added e.lower() & took out raw_input

--- index.py
+++ index.py
@@ -1,32 +1,29 @@
 #!/usr/env/bin python
 
-a= "Hello "
-b= "It is so nice to meet you  "
-c=raw_input("What is your name?")
+a = "Hello "
+b = "It is so nice to meet you  "
+c = input("What is your name?")
 
 print(a+b+c)
 
-d = raw_input("How old are you?")
+d = input("How old are you?")
 print(d + " is a great age!")
 
-e = raw_input("Do You speak English or Spanish?")
-English = "I also speak English."
-Spanish = "Al espanol yo tambien hablo."
+e = input("Do You speak English or Spanish?")
+e = e.lower()
+english = "I also speak English."
+spanish = "Al espanol yo tambien hablo."
 
 
-if e == "English":
-    print(English)
-    print("I am so happy, " + c +" that you are here with me today.")
+if e == "english":
+    print(english)
+    print("I am so happy, " + c + " that you are here with me today.")
     print("Trying to learn Github by myself is hard.")
-if e == "Spanish":
-     print(Spanish)
-     print("Estoy tan feliz" + c + "que hoy estas aqui conmigo.")
-     print("tratar de aprender github por mi cuenta es difficil.")
-if e == "Spanish":
-     print(Spanish)
-elif e != "English" and e != "Spanish":
+if e == "spanish":
+    print(spanish)
+    print("Estoy tan feliz" + c + "que hoy estas aqui conmigo.")
+    print("tratar de aprender github por mi cuenta es difficil.")
+elif e != "english" and e != "spanish":
     print("[-] Please enter either English or Spanish.")
-
-
 
     
